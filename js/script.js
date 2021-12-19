6cbc95a376bc19247334069d227c3aae6fe8205f
samikshyakc1996 <samikshyakc1996@gmail.com>
2021-12-19 03:06:49 -0500

    track got played when selected individually and when clicked next,previous btns

--- js/script.js
+++ js/script.js
@@ -17,9 +17,62 @@ const playlist = [
   `./audio/With A Little Help From My Friends - Remastered 2009.mp3`,
 ];
 
+let playlistIndex = 0;
+const song = new Audio();
+//FUNCTIONS
+const loadSongFromPlaylistByIndex = function (index = 0, start = false) {
+  playlistIndex = index;
+
+  const keepPlaying = !song.paused;
+  //SETUP THE FIRST SONG TO PLAY
+  song.src = playlist[playlistIndex];
+};
 playlist.forEach(function (item, index) {
   playlistEle.innerHTML += `<li data-index="${index}">${item.slice(
     item.indexOf("audio/") + 6
   )}</li>`;
   // SHORTENED THE NAME OF TRACK
+});
+
+window.addEventListener(`load`, function () {
+  // IF PLAY/PAUSE IS CLICKED
+  playOrPause.addEventListener(`click`, function (event) {
+    if (!song.paused) {
+      song.pause();
+      playOrPause.textContent = `▶️`;
+    } else if (song.paused) {
+      song.play();
+
+      playOrPause.textContent = `⏸`;
+    }
+  });
+
+  // WHEN PREVIOUS SONG BUTTON IS CLICKED
+  playprev.addEventListener(`click`, function (event) {
+    playOrPause.textContent = `⏸`;
+
+    playlistIndex =
+      playlistIndex - 1 < 0 ? playlist.length - 1 : playlistIndex - 1;
+    loadSongFromPlaylistByIndex(playlistIndex, true);
+  });
+  // WHEN NEXT SONG BUTTON IS CLICKED
+  playnext.addEventListener(`click`, function (event) {
+    playOrPause.textContent = `⏸`;
+
+    const nextIndex =
+      playlistIndex + 1 > playlist.length - 1 ? 0 : playlistIndex + 1;
+
+    loadSongFromPlaylistByIndex(nextIndex, true);
+  });
+
+  //ADD A LISTENER TO ENTIRE PLAYLIST
+  playlistEle.addEventListener(`click`, function (event) {
+    const songToPlay = event.target;
+    if (event.target.matches(`li`)) {
+      playlistIndex = Number(songToPlay.dataset.index);
+      loadSongFromPlaylistByIndex(playlistIndex, true);
+
+      playOrPause.textContent = `⏸`;
+    }
+  });
 });
